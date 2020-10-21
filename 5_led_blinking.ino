a429356b06a5f1d5f0f112ee9e24ac581e854c91
agin02 <73021196+agin02@users.noreply.github.com>
2020-10-21 23:48:33 +0530

    add appropriate comments to the program so that program can be easily understood by anyone

--- 5_led_blinking.ino
+++ 5_led_blinking.ino
@@ -7,19 +7,19 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                // wait for a second
+  delay(100);                // waiting for a second
   
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(100);   
   
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
+  delay(100);              // waiting for a second
   
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(100);
 
   digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
+  delay(100);              // waiting  for a second
   
   digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
   delay(100);// wait for a second
